8714ff9819bf66105eb8471c81c65fa174e15d20
nick9730 <nikos.koutrenis@gmail.com>
2024-03-19 05:25:13 +0200

    replace folders

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -12,7 +12,7 @@ flex-wrap: wrap;
 overflow: visible;
 margin-top: 10px;
 margin-left: 20px;
-background-image: url('../images/lol.png');
+background-image: url('./public/assets//images/lol.png');
 background-repeat: no-repeat;
 
 
